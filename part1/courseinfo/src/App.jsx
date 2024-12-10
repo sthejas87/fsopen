571ac93911831d4184e7c2b096e11ac2be239fab
Sidharth Thejas <89645460+sthejas87@users.noreply.github.com>
2024-12-10 16:04:19 +0530

    add completed exercises 1.1-1.2

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -1,35 +1,48 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+const Header = (props) => {
+  return <h1>{props.name}</h1>;
+};
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const Part = (props) => {
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <p>
+      {props.name} {props.num}
+    </p>
+  );
+};
 
-export default App
+const Content = (props) => {
+  return (
+    <div>
+      <Part name={props.part[0].name} num={props.part[0].num} />
+      <Part name={props.part[1].name} num={props.part[1].num} />
+      <Part name={props.part[2].name} num={props.part[2].num} />
+    </div>
+  );
+};
+
+const Total = (props) => {
+  return (
+    <p>
+      Number of exercises{" "}
+      {props.part[0].num + props.part[1].num + props.part[2].num}
+    </p>
+  );
+};
+
+const App = () => {
+  const course = "Half Stack application development";
+  const part = [
+    { name: "Fundamentals of React", num: 10 },
+    { name: "Using props to pass data", num: 7 },
+    { name: "State of a component", num: 14 },
+  ];
+  return (
+    <div>
+      <Header name={course} />
+      <Content part={part} />
+      <Total part={part} />
+    </div>
+  );
+};
+
+export default App;
