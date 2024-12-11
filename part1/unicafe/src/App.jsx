75c30d6b1f86652094574e783af639cd6f180167
Sidharth Thejas <89645460+sthejas87@users.noreply.github.com>
2024-12-12 01:31:54 +0530

    add completed exercise 1.6

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,35 +1,38 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
 
-function App() {
-  const [count, setCount] = useState(0)
+const Header = ({ title }) => <h1>{title}</h1>;
+
+const Button = ({ handleClick, text }) => (
+  <button onClick={handleClick}>{text}</button>
+);
+
+const Stat = ({ title, vari }) => (
+  <p>
+    {title} {vari}
+  </p>
+);
+
+const App = () => {
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+  const increaseGood = () => setGood(good + 1);
+  const increaseNeutral = () => setNeutral(neutral + 1);
+  const increaseBad = () => setBad(bad + 1);
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <div>
+      <Header title={"give feedback"} />
+      <Button handleClick={increaseGood} text={"good"} />
+      <Button handleClick={increaseNeutral} text={"neutral"} />
+      <Button handleClick={increaseBad} text={"bad"} />
+      <Header title={"statistics"} />
+      <Stat title={"good"} vari={good} />
+      <Stat title={"neutral"} vari={neutral} />
+      <Stat title={"bad"} vari={bad} />
+    </div>
+  );
+};
 
-export default App
+export default App;
